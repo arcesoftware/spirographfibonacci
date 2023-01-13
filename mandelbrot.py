21a5c5aa4f040aa41d440e798ef9e3fb27c74627
Juan Arce <77249136+arcesoftware@users.noreply.github.com>
2023-01-13 01:57:02 -0600

    Update mandelbrot.py

--- mandelbrot.py
+++ mandelbrot.py
@@ -20,7 +20,14 @@ Overall, this code provides a great example of how the turtle library can be use
 @author: Arce
 """
 import turtle
-
+# Set the screen's background color
+turtle.bgcolor("black")
+# Set the turtle's pen color to white
+turtle.pencolor("white")
+# Set the turtle's pen size to 1 pixel
+turtle.pensize(1)
+# Set the turtle's speed to the maximum
+turtle.speed(0)
 # Set the screen size and the maximum number of iterations
 turtle.screensize(canvwidth=7680, canvheight=4800, bg='black')
 max_iterations = 256
@@ -33,18 +40,6 @@ def escape_time(c):
         if abs(z) > 2:
             return i
     return max_iterations
-
-# Set the screen's background color
-turtle.bgcolor("black")
-
-# Set the turtle's pen color to white
-turtle.pencolor("white")
-
-# Set the turtle's pen size to 1 pixel
-turtle.pensize(1)
-
-# Set the turtle's speed to the maximum
-turtle.speed(0)
 
 # Iterate over the complex plane
 for x in range(-400, 400):
