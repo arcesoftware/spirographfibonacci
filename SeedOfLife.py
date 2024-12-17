81f86ca6c9b5dbd0164e01d1405834f6ed336874
Juan Arce <77249136+arcesoftware@users.noreply.github.com>
2024-12-17 14:34:48 -0600

    SeedOfLife.py

--- SeedOfLife.py
+++ SeedOfLife.py
@@ -1,31 +1,25 @@
-
 import turtle
 import math
-
-turtle.screensize(canvwidth=7680, canvheight=4800, bg='black')
-radius = 180
-
+turtle.screensize(canvwidth=680, canvheight=800, bg='black')
 def seed_of_life(radius):
     skk = turtle.Turtle()
     skk.color("red")
-    skk.speed(3)
-    skk.penup()
-    skk.goto(0, 0)
-    skk.pendown()
-    skk.hideturtle()
-
-    # Rotate the pattern by 45 degrees by adding 45 to the angles
-    initial_rotation = 90
-
-    for i in range(6):
-        angle = i * 60 + initial_rotation
-        x = radius * math.cos(math.radians(angle))
-        y = radius * math.sin(math.radians(angle))
+    skk.speed(0)  # Set maximum speed for faster drawing
+    skk.hideturtle()  # Hide the turtle pointer
+    initial_rotation = 90  # Rotate the pattern by 90 degrees
+    positions = [
+        (
+            radius * math.cos(math.radians(i * 60 + initial_rotation)),
+            radius * math.sin(math.radians(i * 60 + initial_rotation))
+        )
+        for i in range(6)
+    ]
+    # Draw circles
+    for x, y in positions:
         skk.penup()
         skk.goto(x, y)
         skk.pendown()
         skk.circle(radius)
-
     turtle.done()
+seed_of_life(180)
 
-seed_of_life(radius)
